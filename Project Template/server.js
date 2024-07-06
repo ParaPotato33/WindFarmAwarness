41b1e621247b8a30875909a210d5eee3219d8702
Adam Russell <adamdylanrussell@gmail.com>
2024-07-06 14:32:51 +0100

    Update server.js

--- Project Template/server.js
+++ Project Template/server.js
@@ -83,7 +83,7 @@ var server = http.createServer(function (req, res) {
                 res.writeHead(500, { 'Content-Type': 'text/plain' });
                 res.end('Internal server error');
             } else {
-                res.writeHead(200, { 'Content-Type': 'application/javascript' });
+                res.writeHead(200, { 'Content-Type': 'image/png' });
                 res.end(data);
             }
         });
